678a441db0dc0a2fa20a342980ec822e484aa3a6
claudio.gbgomes <claudio.gbgomes@TIT0676436W11-1>
2026-03-13 16:05:49 -0300

    Adiciona classe Ferramenta com validações e relação Usuario
    Inclui a entidade Ferramenta no namespace uc10_Locatem.Model, com propriedades, validações de dados obrigatórios e relacionamento de chave estrangeira com a entidade Usuario. Utiliza anotações de data annotation para garantir integridade e ignora a serialização do objeto Usuario em JSON.

---
+++ uc10-Locatem/Model/Ferramenta.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace uc10_Locatem.Model
+{
+    public class Ferramenta
+    {
+        [Key]
+        public int FerramentaId { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatorio")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Campo Obrigatorio")]
+        public string Marca { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Campo Obrigatorio")]
+        public string Modelo { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Campo Obrigatorio")]
+        public string Descricao { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Campo Obrigatorio")]
+        public string Acessorios { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Campo Obrigatorio")]
+        public int Diaria { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatorio")]
+        public int Caucao { get; set; }
+
+        public DateTime DataCadastro { get; set; }
+
+        public int UsuarioId { get; set; }
+
+        [ForeignKey(nameof(UsuarioId))]
+        [JsonIgnore]
+        public Usuario Usuario { get; set; } = null!;
+
+
+    }
+}
