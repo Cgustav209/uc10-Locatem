76ccc46bd876b12dd59b9da87d9b3ef447ac6e31
Felipe Aragão da Silva <silvaaragao2008@gmail.com>
2026-03-19 16:11:50 -0300

    Api de reservas pronta

--- uc10-Locatem/Controllers/ReservasController.cs
+++ uc10-Locatem/Controllers/ReservasController.cs
@@ -89,18 +89,82 @@ namespace uc10_Locatem.Controllers
             return Ok(reserva);
         }
 
+        //[Authorize]
         [HttpPut("cancelar/{id}")]
-        public async Task<IActionResult> CancelarReserva(int id, [FromBody] int usuarioId)
+        public async Task<IActionResult> CancelarReserva(int id)
         {
-            var (sucesso, mensagem) = await _reservaService.CancelarReserva(id, usuarioId);
-            if (!sucesso)
-            {
-                return BadRequest(mensagem);
-            }
-            return Ok(mensagem);
+            // Aqui estou pegando o id do usuário logado para verificar se ele é o dono da reserva que está tentando cancelar
+            var usuarioId = int.Parse(User.FindFirst("id").Value);
 
+            // Chama o método CancelarReserva do services de reservas, passando o id da reserva e o id do usuário logado
+            var (sucesso, mensagem) = await _reservaService.CancelarReserva(id, usuarioId);
+
+            // Se o cancelamento não for bem-sucedido, retorna um erro 400 com a mensagem de erro
+            if (!sucesso)
+                return BadRequest(mensagem);
+
+            // Se o cancelamento for bem-sucedido, retorna um status 200 com a mensagem de sucesso
+            return Ok(mensagem);
 
         }
 
+        //[Authorize]
+        [HttpPut("aceitar/{id}")]
+        public async Task<IActionResult> AceitarReserva(int id)
+        {
+            // Aqui estou pegando o id do usuário logado para verificar se ele é o dono da ferramenta relacionada à reserva que está tentando aceitar
+            var usuarioId = int.Parse(User.FindFirst("id").Value);
+
+            // Chama o método AceitarReserva do serviço de reservas, passando o id da reserva e o id do usuário logado
+            var (sucesso, mensagem) = await _reservaService.AceitarReserva(id, usuarioId);
+
+            // Se a aceitação não for bem-sucedida, retorna um erro 400 com a mensagem de erro
+            if (!sucesso)
+                return BadRequest(mensagem);
+            // Se a aceitação for bem-sucedida, retorna um status 200 com a mensagem de sucesso
+            return Ok(mensagem);
+
+        }
+
+        //[Authorize]
+        [HttpPut("recusar/{id}")]
+        public async Task<IActionResult> RecusarReserva(int id)
+        {
+            // Aqui estou pegando o id do usuário logado para verificar se ele é o dono da ferramenta relacionada à reserva que está tentando recusar
+            var usuarioId = int.Parse(User.FindFirst("id").Value);
+            // Chama o método RecusarReserva do serviço de reservas, passando o id da reserva e o id do usuário logado
+            var (sucesso, mensagem) = await _reservaService.RecusarReserva(id, usuarioId);
+            // Se a recusa não for bem-sucedida, retorna um erro 400 com a mensagem de erro
+            if (!sucesso)
+                return BadRequest(mensagem);
+            // Se a recusa for bem-sucedida, retorna um status 200 com a mensagem de sucesso
+            return Ok(mensagem);
+        }
+
+        //[Authorize]
+        [HttpGet("minhasReservas")]
+        public async Task<IActionResult> MinhasReservas()
+        {
+            // Aqui estou pegando o id do usuário logado para buscar as reservas relacionadas a ele
+            var usuarioId = int.Parse(User.FindFirst("id").Value);
+
+            // Chama o método ListarReservasDoUsuario do service de reservas, passando o id do usuário logado para obter a lista de reservas relacionadas a ele
+            var reservas = await _reservaService.ListarReservasDoUsuario(usuarioId);
+
+            // Retorna a lista de reservas do usuário logado
+            return Ok(reservas);
+        }
+
+        //[Authorize]
+        [HttpGet("reservasRecebidas")]
+        public async Task<IActionResult> ReservasRecebidas()
+        {
+            // Aqui estou pegando o id do usuário logado para buscar as reservas relacionadas às ferramentas que ele possui
+            var usuarioId = int.Parse(User.FindFirst("id").Value);
+            // Chama o método ListarReservasRecebidas do service de reservas, passando o id do usuário logado para obter a lista de reservas relacionadas às ferramentas que ele possui
+            var reservas = await _reservaService.ListarResrvasRecebidas(usuarioId);
+            // Retorna a lista de reservas recebidas para as ferramentas do usuário logado
+            return Ok(reservas);
+        }
     }
 }
--- uc10-Locatem/Services/ReservaService.cs
+++ uc10-Locatem/Services/ReservaService.cs
@@ -1,5 +1,7 @@
-﻿using uc10_Locatem.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using uc10_Locatem.Data;
 using uc10_Locatem.Enum;
+using uc10_Locatem.Model;
 
 namespace uc10_Locatem.Services
 {
@@ -45,6 +47,77 @@ namespace uc10_Locatem.Services
             return (true, "Reserva cancelada com sucesso.");
 
 
+        }
+
+
+
+        //Aceitar reserva - Somente o dono da ferramenta pode aceitar a reserva 
+        public async Task<(bool sucesso, string mensagem)> AceitarReserva(int reservaId, int usuarioId)
+        {
+            // Carregar a reserva junto com o produto relacionado para verificar o dono da ferramenta
+            var reserva = await _context.Reserva.Include(r => r.Produto).FirstOrDefaultAsync(r => r.Id == reservaId);
+
+            // Verificar se a reserva existe
+            if (reserva == null)
+            {
+                return (false, "Reserva não encontrada.");
+            }
+            // Verificar se o usuário é o dono da ferramenta
+            var produto = await _context.Produto.FindAsync(reserva.FerramentaId);
+            if (produto.UsuarioId != usuarioId)
+            {
+                return (false, "Você não tem permissão para aceitar esta reserva.");
+            }
+            // Verificar se a reserva já foi aceita ou recusada
+            if (reserva.Status != StatusReserva.Pendente)
+            {
+                return (false, "A reserva já foi processada e não pode ser aceita.");
+            }
+            reserva.Status = StatusReserva.Aceita;
+            await _context.SaveChangesAsync();
+            return (true, "Reserva aceita com sucesso.");
+        }
+
+
+
+        //Recusar reserva - apenas o dono da ferramenta pode recusar a reserva, e somente se a reserva estiver pendente
+        public async Task<(bool sucesso, string mensagem)> RecusarReserva(int reservaId, int usuarioId)
+        {
+            // Carregar a reserva junto com o produto relacionado para verificar o dono da ferramenta
+            var reserva = await _context.Reserva.Include(r => r.Produto).FirstOrDefaultAsync(r => r.Id == reservaId);
+
+            // Verificar se a reserva existe
+            if (reserva == null)
+            {
+                return (false, "Reserva não encontrada.");
+            }
+            // Verificar se o usuário é o dono da ferramenta
+            if (reserva.produto.UsuarioId != usuarioId)
+            {
+                return (false, "Você não tem permissão para recusar esta reserva.");
+            }
+            // Verificar se a reserva já foi aceita ou recusada
+            if (reserva.Status != StatusReserva.Pendente)
+            {
+                return (false, "A reserva já foi processada e não pode ser recusada.");
+            }
+
+            // Atualizar o status da reserva para recusada
+            reserva.Status = StatusReserva.Recusada;
+
+            await _context.SaveChangesAsync();
+            return (true, "Reserva recusada com sucesso.");
+        }
+
+        public async Task<List<Reserva>> ListarReservasDoUsuario(int usuarioId)
+        {
+            // Carregar as reservas do usuário junto com os produtos relacionados para exibir detalhes da ferramenta
+            return await _context.Reserva.Include(r => r.Produto).Where(r => r.UsuarioId == usuarioId).ToListAsync();
+        }
+
+        public async Task<List<Reserva>> ListarResrvasRecebidas(int usuarioId)
+        {
+            return await _context.Reserva.Include(r => r.Usuario).Where(r => r.Produto.UsuarioId == usuarioId).ToListAsync();
         }
     }
 }
