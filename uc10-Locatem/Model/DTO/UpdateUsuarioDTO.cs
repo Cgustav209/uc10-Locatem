5d4009e74f84aaac0dc22af63ac3b35164dd53be
Stephany <tete.calmon@hotmail.com>
2026-04-28 16:16:37 -0300

    Agora o usuario pode editar nome, telefone ou email.

---
+++ uc10-Locatem/Model/DTO/UpdateUsuarioDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace uc10_Locatem.Model.DTO
+{
+    public class UpdateUsuarioDTO
+    {
+        public int Id { get; set; }
+       
+        public string? Nome { get; set; }
+
+        [RegularExpression(@"^\(\d{2}\)\s?\d{4,5}-\d{4}$",
+            ErrorMessage = "Telefone deve estar no formato (11) 99999-9999")]
+        public string? Telefone { get; set; }
+
+        [EmailAddress(ErrorMessage = "Email inválido")]
+        public string? Email { get; set; }
+
+    }
+}
